50faed4f47ac591fc02ebfd491c42eeea5d665a0
Louisa Dayton <dayton.louisa@gmail.com>
2019-05-04 12:03:00 -0600

    first commit

---
+++ static/_vti_cnf/test.php
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|21 Mar 2019 16:55:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|GILGALAD\\Matthew Cloward
+vti_modifiedby:SR|GILGALAD\\Matthew Cloward
+vti_timecreated:TR|21 Mar 2019 16:55:36 -0000
+vti_cacheddtm:TX|21 Mar 2019 16:55:36 -0000
+vti_filesize:IR|311
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_backlinkinfo:VX|
---
+++ static/js/getRadioVal.js
@@ -0,0 +1,14 @@
+function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+    
+    // loop through list of radio buttons
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { // radio checked?
+            val = radios[i].value; // if so, hold its value in val
+            break; // and break out of for loop
+        }
+    }
+    return val; // return value of checked radio or undefined if none checked
+}
