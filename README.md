a9b1b4037bc331098b1c69091dfd5f70d0923e3e
Louisa Dayton <dayton.louisa@gmail.com>
2019-05-04 12:21:59 -0600

    Second commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Problems 
+
+We have this lines: 
+
+```js
+app.use('/', express.static(path.join(__dirname, 'static')))
+```
+And the code the connects to SQL Server. 
+
+The code works separately, that is, if I comment out the `app.use...` line, it'll connect to the database. Otherwise, it won't. I'm not sure if these two pieces of code need to be in separate files, or if I just have a basic JavaScript error that I could fix if I started learning JavaScript from the beginning. 
+
+Thanks for all your help! I'll keep searching, and I'll let you know what solutions (if any) I find! 
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = 3000
 app.listen(port)
 
 //app.get('/', (req, res) => res.send('Hello World!')) //Prints Hello World! to the page
-
-//app.listen(port, ()=>console.log(path.join(__dirname, 'static'))) //prints path to console
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.listen(port, ()=>console.log(path.join(__dirname, 'static'))) //prints path to console
 
 //  app.get('/', function (req, res) {
 //      res.send('Hello World!')
@@ -21,15 +21,14 @@ app.listen(port)
 //     res.send('Got a PUT request at /user')
 //   })
   
-// app.get('/', async (req, res) => {
-//     await sql.connect('mssql://SA:Constitution1787@localhost/PolyScore')
-//     const result = await sql.query`select Name from sys.Databases`
-//     console.dir(result)
-//     console.log(result)
-//     sql.close() 
-//     app.use('/', express.static(path.join(__dirname, 'static')))
-//     res.send("Hello, World!"); //loads static folder
-// });
+app.get('/', async (req, res) => {
+    await sql.connect('mssql://SA:Constitution1787@localhost/PolyScore')
+    const result = await sql.query`select Name from sys.Databases`
+    console.dir(result)
+    console.log(result)
+    sql.close() 
+    res.send("Hello, World!"); //loads static folder
+});
 // app.listen(3000, function () {
 //   console.log('Example app listening on port 3000!');
 // });
@@ -46,60 +45,60 @@ app.listen(port)
 //     }
 // }
 
-const Sequelize = require('sequelize'); 
+// const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize('PolyScore', 'SA', 'Constitution1787', {
-    host: 'localhost',
-    dialect: 'mssql',
-    freezeTableName: true,
-    define: {
-      // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-      // This was true by default, but now is false by default
-      timestamps: false
-    }
-    //logging: 'true'
-});
+// const sequelize = new Sequelize('PolyScore', 'SA', 'Constitution1787', {
+//     host: 'localhost',
+//     dialect: 'mssql',
+//     freezeTableName: true,
+//     define: {
+//       // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+//       // This was true by default, but now is false by default
+//       timestamps: false
+//     }
+//     //logging: 'true'
+// });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection established.');
-    })
-    .catch(err => {
-        console.error('Unable to connect: ', err);
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection established.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect: ', err);
+//     });
 
-const Model = Sequelize.Model;
-class User extends Model {}
-User.init({
-  // attributes
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: Sequelize.STRING
-    // allowNull defaults to true
-  }
-}, {
-  sequelize,
-  modelName: 'tiger'
-  // options
-});
+// const Model = Sequelize.Model;
+// class User extends Model {}
+// User.init({
+//   // attributes
+//   firstName: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   lastName: {
+//     type: Sequelize.STRING
+//     // allowNull defaults to true
+//   }
+// }, {
+//   sequelize,
+//   modelName: 'tiger'
+//   // options
+// });
 
-User.sequelize.sync({ force: true }).then(() => {
-    // Now the `users` table in the database corresponds to the model definition
-    return User.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
+// User.sequelize.sync({ force: true }).then(() => {
+//     // Now the `users` table in the database corresponds to the model definition
+//     return User.create({
+//       firstName: 'John',
+//       lastName: 'Hancock'
+//     });
+//   });
 
   
 
- User.findAll().then(tigers => {
-   console.log("All users:", JSON.stringify(tigers, null, 2));
- });
+//  User.findAll().then(tigers => {
+//    console.log("All users:", JSON.stringify(tigers, null, 2));
+//  });
 
 // User.create({ firstName: "Jane", lastName: "Doe" }).then(jane => {
 //   console.log("Jane's auto-generated ID:", jane.id);
