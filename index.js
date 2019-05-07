affea03d9323d19a7d95d73bbb5926860b38a5ce
Louisa Dayton <dayton.louisa@gmail.com>
2019-05-07 13:22:40 -0600

    added comments

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var express = require('express');
 const path = require('path')
 var app = express();
 const port = 3000 
-//app.listen(port)
 
-//app.get('/', (req, res) => res.send('Hello World!')) //Prints Hello World! to the page
+//THESE TWO LINES WILL OPEN THE PORT AND OUTPUT "HELLO, WORLD!"
+app.listen(port)
+app.get('/', (req, res) => res.send('Hello World!')) //Prints Hello World! to the page
+
+//THESE TWO LINES WILL UPLOAD JUSTIN'S FILES AS STATIC FILES
 //app.use('/', express.static(path.join(__dirname, 'static')))
 //app.listen(port, ()=>console.log(path.join(__dirname, 'static'))) //prints path to console
 
 
-
+//EXPERIMENTAL CODE FROM EXPRESS WEBSITE. 
 //  app.get('/', function (req, res) {
 //      res.send('Hello World!')
 //    })
@@ -23,6 +26,7 @@ const port = 3000
 //     res.send('Got a PUT request at /user')
 //   })
   
+//THIS CODE CONNECTS TO THE SQL SERVER DATABASE
 // app.get('/', async (req, res) => {
 //     sql.close()
 //     await sql.connect('mssql://SA:Constitution1787@localhost/PolyScore')
@@ -48,61 +52,73 @@ const port = 3000
 //     }
 // }
 
-const Sequelize = require('sequelize'); 
+//UNCOMMENT ALL THE CODE BELOW TO USE SEQUELIZER TO CONNECT WITH SQL SERVER
+// const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize('PolyScore', 'SA', 'Constitution1787', {
-    host: 'localhost',
-    dialect: 'mssql',
-    freezeTableName: true,
-    define: {
-      // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-      // This was true by default, but now is false by default
-      timestamps: false
-    }
-    //logging: 'true'
-});
+// const sequelize = new Sequelize('PolyScore', 'SA', 'Constitution1787', {
+//     host: 'localhost',
+//     dialect: 'mssql',
+//     freezeTableName: true,
+//     define: {
+//       // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+//       // This was true by default, but now is false by default
+//       timestamps: false
+//     }
+//     //logging: 'true'
+// });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection established.');
-    })
-    .catch(err => {
-        console.error('Unable to connect: ', err);
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection established.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect: ', err);
+//     });
 
-const Model = Sequelize.Model;
-class User extends Model {}
-User.init({
-  // attributes
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  bestFriend: {
-    type: Sequelize.STRING
-    // allowNull defaults to true
-  }
-}, {
-  sequelize,
-  modelName: 'tiger'
-  // options
-});
+// const Model = Sequelize.Model;
+// class User extends Model {}
+// User.init({
+//   // attributes
+//   firstName: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   bestFriend: {
+//     type: Sequelize.STRING
+//     // allowNull defaults to true
+//   }
+// }, {
+//   sequelize,
+//   modelName: 'tiger'
+//   // options
+// });
 
-User.sequelize.sync().then(() => {
-    // Now the `users` table in the database corresponds to the model definition
-    return User.create({
-      firstName: 'Hobbes',
-      bestFriend: 'Calvin'
-    });
-  });
+// User.sequelize.sync().then(() => {
+//     Now the `users` table in the database corresponds to the model definition
+//     return User.create({
+//       firstName: 'Hobbes',
+//       bestFriend: 'Calvin'
+//     });
+//   });
 
-  
-
- User.findAll().then(tiger => {
-   console.log("All users:", JSON.stringify(tiger, null, 3));
- });
-
-// User.create({ firstName: "Jane", lastName: "Doe" }).then(jane => {
+// User.create({ firstName: "Jane", bestFriend: "John" }).then(jane => {
 //   console.log("Jane's auto-generated ID:", jane.id);
 // });
+
+//  User.findAll().then(tiger => {
+//    console.log("All users:", JSON.stringify(tiger, null, 3));
+//  });
+
+//  User.destroy({
+//     where: {},
+//     truncate : true
+//   }).then(() => {
+//     console.log("Done");
+//   });
+
+// User.update({ bestFriend: null }, {
+//     where: {}
+//   }).then(() => {
+//     console.log("Done");
+//   });
