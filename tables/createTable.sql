22defa00cc6c67f16c9dd9c1cfdaf33017afe2db
mpage21 <madmadsoccergirl@hotmail.com>
2020-02-07 13:42:56 -0700

    Updated createTable and other data files

--- tables/createTable.sql
+++ tables/createTable.sql
@@ -1,4 +1,4 @@
-SET @query = CONCAT('CREATE TABLE ', @tblName, ' ( id smallint unsigned not null, snp varchar(20), chromosome tinyint,  hg38 int, hg19 int, hg18 int, hg17 int, gene varchar(50), ethnicity varchar(50), raf float, riskAllele varchar(20), pValue double, oddsRatio float, lowerCI float, upperCI float, seLnOR double, study varchar(50), cohort double, citations double)');
+SET @query = CONCAT('CREATE TABLE ', @tblName, ' ( id smallint unsigned not null, snp varchar(20), hg38 varchar(50), hg19 varchar(50), hg18 varchar(50), hg17 varchar(50), gene varchar(50), ethnicity varchar(100), raf float, riskAllele varchar(20), pValue double, oddsRatio float, lowerCI float, upperCI float, seLnOR double, study varchar(50), cohort double, citations double)');
 PREPARE stmt FROM @query;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
