2cb05137ebd4cb477f043227ec3414034614c13f
root <root@lsprs.byu.edu>
2020-02-29 21:02:41 -0700

    fixed adhd table finally

--- static/js/sharedCode.js
+++ static/js/sharedCode.js
@@ -8,18 +8,18 @@
     diseasesAndStudies['ALS'] = ['Ahmeti et al. 2012 (Joint)', 'Ahmeti et al. 2012 (Meta)', 'Diekstra et al. 2014', 'Laaksovirta et al. 2010',
         'Landers et al. 2009', 'van Es MA et al. 2007', 'van Rheenen W et al. 2016 (High Impact)'];
     diseasesAndStudies['DEP'] = ['Ripke et al. 2012 (High Impact)', 'Wray et al. 2018 (Largest Cohort)'];
-    diseasesAndStudies['CORONARYHEARTDISEASE'] = ['Coronary Artery Disease (C4D) Genetics Consortium 2011', 'Samani NJ 2007', 'Schunkert H 2011',
-        'Wild PS 2011'];
-    diseasesAndStudies['HF'] = ['Shah et. al 2020'];
-    diseasesAndStudies['T1D'] = ['Todd JA 2007', 'Wellcome Trust Case Control Consortium 2007', 'Hakonarson H 2007', 'Grant SF 2008',
-        'Cooper JD 2008', 'Barrett JC 2009', 'Plagnol V 2011', 'Tomer Y 2015', 'Onengut-Gumuscu S 2015', 'Sharma A 2018', 'Charmet R 2018',
-        'Kawabata Y 2018', 'Zhu M 2019'];
-    diseasesAndStudies['T2D'] = ['Mahajan A 2018', 'Scott LJ 2007', 'Zeggini E 2007', 'Saxena R 2007', 'Wellcome Trust Case Control Consortium 2007',
-        'Zeggini E 2008', 'Takeuchi F 2009', 'Rung J 2009', 'Voight BF 2010', 'Shu XO 2010', 'Sim X 2011', 'Parra EJ 2011', 'Kooner JS 2011',
-        'Cho YS 2011', 'Palmer ND 2012', 'Perry JR 2012', 'Li H 2012', 'Tabassum R 2012', 'Saxena R 2013', 'Hara K 2013', 'Mahajan A 2014',
-        'Ng MC 2014', 'Ghassibe-Sabbagh M 2014', 'Huang KC 2015', 'Imamura M 2016', 'Cook JP 2016', 'Qi Q 2017', 'Scott RA 2017',
-        'Zhao W 2017', 'Morris AP 2012', 'Bonàs-Guarch S 2018', 'Domínguez-Cruz MG 2018', 'Wood AR 2016', 'Xue A 2018', 'Suzuki K 2019',
-        'Chen J 2019', 'Flannick J 2019', 'Mahajan A 2018'];
+    //diseasesAndStudies['CORONARYHEARTDISEASE'] = ['Coronary Artery Disease (C4D) Genetics Consortium 2011', 'Samani NJ 2007', 'Schunkert H 2011',
+     //   'Wild PS 2011'];
+    //diseasesAndStudies['HF'] = ['Shah et. al 2020'];
+    //diseasesAndStudies['T1D'] = ['Todd JA 2007', 'Wellcome Trust Case Control Consortium 2007', 'Hakonarson H 2007', 'Grant SF 2008',
+    //    'Cooper JD 2008', 'Barrett JC 2009', 'Plagnol V 2011', 'Tomer Y 2015', 'Onengut-Gumuscu S 2015', 'Sharma A 2018', 'Charmet R 2018',
+    //    'Kawabata Y 2018', 'Zhu M 2019'];
+    //diseasesAndStudies['T2D'] = ['Mahajan A 2018', 'Scott LJ 2007', 'Zeggini E 2007', 'Saxena R 2007', 'Wellcome Trust Case Control Consortium 2007',
+    //    'Zeggini E 2008', 'Takeuchi F 2009', 'Rung J 2009', 'Voight BF 2010', 'Shu XO 2010', 'Sim X 2011', 'Parra EJ 2011', 'Kooner JS 2011',
+    //    'Cho YS 2011', 'Palmer ND 2012', 'Perry JR 2012', 'Li H 2012', 'Tabassum R 2012', 'Saxena R 2013', 'Hara K 2013', 'Mahajan A 2014',
+    //    'Ng MC 2014', 'Ghassibe-Sabbagh M 2014', 'Huang KC 2015', 'Imamura M 2016', 'Cook JP 2016', 'Qi Q 2017', 'Scott RA 2017',
+    //    'Zhao W 2017', 'Morris AP 2012', 'Bonàs-Guarch S 2018', 'Domínguez-Cruz MG 2018', 'Wood AR 2016', 'Xue A 2018', 'Suzuki K 2019',
+    //    'Chen J 2019', 'Flannick J 2019', 'Mahajan A 2018'];
 
     //freeze the object so it can't be edited by the browser or server
     diseasesAndStudies = Object.freeze(diseasesAndStudies);
