9e0babb2c99fba8099157b3faaad09e485e64a34
Matthew Cloward <mattcloward@gmail.com>
2020-04-24 12:40:13 -0500

    Fixing issues with gitignore

--- static/js/passwords.js
+++
@@ -1,6 +0,0 @@
-(function (exports) {
-    exports.getMySQLPassword = function() {
-        return '[Miller20] virus hope advance'
-    }
-
-})(typeof exports === 'undefined' ? this['passwords'] = {} : exports);
