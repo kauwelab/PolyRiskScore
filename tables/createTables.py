feafe70a1408b5b5eb7c349cd0992f8cdd5ee26a
Matthew Cloward <mattcloward@gmail.com>
2020-05-28 15:17:04 -0500

    Fixed the createTables.py file

--- tables/createTables.py
+++ tables/createTables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
- 
+
 import mysql.connector
 from mysql.connector import errorcode
 from mysql.connector.constants import ClientFlag
@@ -8,12 +8,17 @@ import os
 from os import listdir
 from os.path import isfile, join
 from sys import argv
- 
-import datetime, time
 
+import datetime
+import time
+
+# creates a connection to the MySQL database using the given config dictionary
+# The config should be given in the following form:
+#{'user': 'the user(polyscore)', 'password': argv[1], 'host': 'localhost', 'database': 'the database(polyscore)', 'allow_local_infile': True, 'auth_plugin': 'mysql_native_password',}
 def getConnection(config):
     try:
         connection = mysql.connector.connect(**config)
+        # sets all queries to commit automatically so commiting is not required
         connection.autocommit = True
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -25,81 +30,86 @@ def getConnection(config):
     else:
         return connection
 
-def checkTableExists(connection, tableName):
-    cursor = connection.cursor()
+# returns true if 'tableName' exists in the MySQL database
+def checkTableExists(cursor, tableName):
+    exists = False
     cursor.execute("""
         SELECT COUNT(*)
         FROM information_schema.tables
         WHERE table_name = '{0}'
         """.format(tableName.replace('\'', '\'\'')))
     if cursor.fetchone()[0] == 1:
-        cursor.close()
-        return True
+        exists = True
 
     cursor.close()
-    return False
+    return exists
 
-def deleteTable(connection, dbTableName):
-    cursor = connection.cursor()
-    sql = "DROP TABLE " + dbTableName + ";"
+# deletes 'dbTableName' from the database
+def deleteTable(cursor, dbTableName):
+    sql = "DROP TABLE `" + dbTableName + "`;"
     cursor.execute(sql)
     cursor.close()
 
-def createTable(connection, dbTableName):
-    cursor = connection.cursor()
-    #TODO CHARACTER SET utf8 COLLATE utf8_general_ci
-    tableColumns =" ( id smallint unsigned not null, snp varchar(20), hg38 varchar(50), hg19 varchar(50), hg18 varchar(50), hg17 varchar(50), gene varchar(50), raf float, riskAllele varchar(20), pValue double, oddsRatio float, lowerCI float, upperCI float, study varchar(50), studyID varchar(20))';"
-    sql = "set names utf8mb4; SET @query = 'CREATE TABLE " + dbTableName + tableColumns + "PREPARE stmt FROM @query;" + "EXECUTE stmt;" + "DEALLOCATE PREPARE stmt;"
-    cursor.execute(sql, multi = True)
+# creates 'dbTableName' and adds it to the database with no data
+def createTable(cursor, dbTableName):
+    # TODO CHARACTER SET utf8 COLLATE utf8_general_ci
+    tableColumns = " ( id smallint unsigned not null, snp varchar(20), hg38 varchar(50), hg19 varchar(50), hg18 varchar(50), hg17 varchar(50), gene varchar(50), raf float, riskAllele varchar(20), pValue double, oddsRatio float, lowerCI float, upperCI float, study varchar(50), studyID varchar(20))';"
+    sql = "set names utf8mb4; SET @query = 'CREATE TABLE `" + dbTableName + "`" + tableColumns + "PREPARE stmt FROM @query;" + "EXECUTE stmt;" + "DEALLOCATE PREPARE stmt;"
+    cursor.execute(sql, multi=True)
     cursor.close()
 
-#removes the table in fileNames if it exists and creates a new table
+# removes the table in fileNames if it exists and creates a new table
 def createFreshTable(config, tableName, dbTableName):
     connection = getConnection(config)
     dropped = False
     # drop the table if it already exists
-    if checkTableExists(connection, dbTableName):
-        deleteTable(connection, dbTableName)
+    if checkTableExists(connection.cursor(), dbTableName):
+        deleteTable(connection.cursor(), dbTableName)
         dropped = True
-    #create a new table with the table columns specified
-    createTable(connection, dbTableName)
+    # create a new table with the table columns specified
+    createTable(connection.cursor(), dbTableName)
     if dropped:
         print(dbTableName + " recreated")
     else:
         print(dbTableName + " created")
+    connection.close()
 
-def addDataToTable(config, tableName, dbTableName):
+# adds 'tableName' csv data to the 'dbTableName' table of the database
+def addDataToTable(config, associationTablesFolderPath, tableName, dbTableName):
     connection = getConnection(config)
     cursor = connection.cursor()
-    workingDirectoryPath = os.getcwd().replace("\\", "/") + "/"
-    sql = "LOAD DATA LOCAL INFILE '" + workingDirectoryPath + tableName + ".csv' INTO TABLE " + dbTableName + " CHARACTER SET latin1 COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;"            
-    cursor.execute(sql, multi = True)
+    path = associationTablesFolderPath + tableName + ".csv"
+    path = path.replace("\\", "/")
+    # character set latin1 is required for some of the tables containing non English characters in their names
+    sql = 'LOAD DATA LOCAL INFILE "' + path + '" INTO TABLE `' + dbTableName + '`CHARACTER SET latin1 COLUMNS TERMINATED BY "," LINES TERMINATED BY "\n" IGNORE 1 LINES;'
+    cursor.execute(sql, multi=True)
+    print(dbTableName + " data added")
     cursor.close()
-    print(tableName + " data added")
+    connection.close()
 
-def getAllExistingTables(config):
-    connection = getConnection(config)
-    cursor = connection.cursor()
+#gets the names of all the tables in the database
+def getAllExistingTables(cursor):
     cursor.execute("SHOW tables")
     existingTableNames = []
     for (table_name,) in cursor:
         existingTableNames.append(table_name)
+    cursor.close()
     return existingTableNames
 
-#gets the trait name formated for website and database use
-  #all lowercase, spaces to underscores, forward slashes to dashes, and no commas
+# gets the trait name formated for website and database use
+  # all lowercase, spaces to underscores, forward slashes to dashes, and no commas or apostrophies
 def getDatabaseTableName(traitName):
-    dbTableName = traitName.lower().replace(" ", "_").replace(",", "").replace("/", "-")[:64]
+    dbTableName = traitName.lower().replace(" ", "_").replace(
+        ",", "").replace("/", "-").replace("'", "")[:64]
     return(dbTableName)
 
-def enableLocalLoad(config):
-    connection = getConnection(config)
-    cursor = connection.cursor()
+#enables tables to be loaded from local files temporarily
+def enableLocalLoad(cursor):
     sql = "SET GLOBAL local_infile = 1;"
     cursor.execute(sql)
     cursor.close()
-    connection.close()
 
+#prints the usage statement
 def usage():
     print("""Usage: password cleanDatabaseBoolean commaSeparatedTableString
     password- the MySQL password
@@ -107,63 +117,67 @@ def usage():
     commaSeparatedTableString- optional list of table names to update, separated by a comma and a space (ex: "ad, als")
     """)
 
+
 def main():
-    config = {}
-    #TODO
-    if len(argv) > 1:
-        config = {
+    associationTablesFolderPath = os.getcwd().replace("\\", "/") + "/association_tables/"
+    #if no password is provided, as for one and close
+    if len(argv) <= 1:
+        print("Password is required!")
+        usage()
+        exit()
+    #set default variables
+    config = {
             'user': 'polyscore',
             'password': argv[1],
             'host': 'localhost',
             'database': 'polyscore',
             'allow_local_infile': True,
             'auth_plugin': 'mysql_native_password',
-            #'raise_on_warnings': True,
         }
-    else:
-        print("Password is required!")
-        usage()
-        exit()
-
     tablesToUpdate = []
+    connection = getConnection(config)
+
+    #if argv[2] is "true" or "t", removes all the tables in the database not listed in argv[3] or if argv[3] is not specified,
+    #removes the all the tables in the database that are not csvs in the associationTablesFolderPath folder
     if len(argv) > 2:
         if argv[2].lower() == "true" or argv[2].lower() == "t":
-            existingTables = getAllExistingTables(config)
+            existingTables = getAllExistingTables(connection.cursor())
             for tableName in tablesToUpdate:
                 dbTableName = getDatabaseTableName(tableName)
                 if dbTableName in existingTables:
                     existingTables.remove(dbTableName)
-            print("You are about to remove the following tables from the database " + str(existingTables))
+            print(
+                "You are about to remove the following tables from the database " + str(existingTables))
             if input("Are you sure? (y/n)") != "y":
                 print("Aborted by user.")
                 exit()
             else:
                 print('Cleaning the database')
                 for dbTableName in existingTables:
-                    connection = getConnection(config)
-                    deleteTable(connection, dbTableName)
-                    connection.close()
+                    deleteTable(connection.cursor(), dbTableName)
                     print(dbTableName + " removed")
+    #gets the names of the tables to update from argv[3], or if there is no argv[3], 
+    #gets the tables to update from the csvs in the associationTablesFolderPath folder
     if len(argv) > 3:
-        tablesToUpdate = argv[3].split(', ') 
+        tablesToUpdate = argv[3].split(', ')
         print("Updating tables: " + str(tablesToUpdate))
     else:
-        mypath = os.getcwd()
-        tablesToUpdate = [f[:-4] for f in listdir(mypath) if f[-4:] == ".csv"]
+        tablesToUpdate = [
+            f[:-4] for f in listdir(associationTablesFolderPath) if f[-4:] == ".csv"]
         print("Using default tables found in the current working directory.")
-                
 
-    #before adding data, sets a parameter so data can be loaded from local files
-    enableLocalLoad(config)
+    # before adding data, sets a parameter so data can be loaded from local files
+    enableLocalLoad(connection.cursor())
+    connection.close()
 
-    #for each table, create a fresh table and add the data in the current directory with that table name to it
+    # for each table, create a fresh table and add the data in the current directory with that table name to it
     for tableName in tablesToUpdate:
-        #TODO
         dbTableName = getDatabaseTableName(tableName)
-        #print("starting", tableName, dbTableName)
-
         createFreshTable(config, tableName, dbTableName)
-        #addDataToTable(config, tableName, dbTableName)
-    
+        addDataToTable(config, associationTablesFolderPath,
+                       tableName, dbTableName)
+    print("Done!")
+
+
 if __name__ == "__main__":
     main()
