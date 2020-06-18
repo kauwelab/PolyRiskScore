1eba9edaf4abf1c108f0b496e50ee72516bf9125
Matthew Cloward <mattcloward@gmail.com>
2020-06-18 15:29:17 -0500

    fixed require error on caculate_score page

--- static/js/formatHelper.js
+++ static/js/formatHelper.js
@@ -1,18 +1,20 @@
+(function (exports) {
+    exports.formatForTableName = function (traitName) {
+        // formatting trait names for database use
+        // all lowercase, spaces to underscores, forward slashes to dashes, no commas or apostrophies
+        const spacesRegex = / /g;
+        const forwardSlashesRegex = /\//g;
+        const commaRegex = /,/g;
+        const apostrophiesRegex = /'/g;
+        const quoteRegex = /"/g;
 
-exports.formatForTableName = function (traitName) {
-    // formatting trait names for database use
-    // all lowercase, spaces to underscores, forward slashes to dashes, no commas or apostrophies
-    const spacesRegex = / /g;
-    const forwardSlashesRegex = /\//g;
-    const commaRegex = /,/g;
-    const apostrophiesRegex = /'/g;
-    const quoteRegex = /"/g;
+        traitName = traitName.toLowerCase().replace(spacesRegex, "_").replace(commaRegex, "").replace(forwardSlashesRegex, "-").replace(apostrophiesRegex, "").replace(quoteRegex, "");
 
-    traitName = traitName.toLowerCase().replace(spacesRegex, "_").replace(commaRegex, "").replace(forwardSlashesRegex,"-").replace(apostrophiesRegex, "").replace(quoteRegex, "");
+        if (traitName.length > 64) {
+            traitName = traitName.slice(0, 64)
+        }
 
-    if (traitName.length > 64) {
-        traitName = traitName.slice(0,64)
+        return traitName
     }
 
-    return traitName
-}
+})(typeof exports === 'undefined' ? this['sharedCode'] = {} : exports);
