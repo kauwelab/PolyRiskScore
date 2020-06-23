54b3660f05cc8c0df999042de1a9891d6d2002d3
Maddy <madmadsoccergirl@hotmail.com>
2020-06-23 09:47:15 -0600

    fixed accessing the traits in getTraits

--- static/js/calculate_score.js
+++ static/js/calculate_score.js
@@ -1,5 +1,3 @@
-const formatter = require('./formatHelper')
-
 var resultJSON = "";
 //TODO gzip and zip still need work
 var validExtensions = ["vcf", "gzip", "zip"]
@@ -13,17 +11,14 @@ function getTraits() {
         type: "GET",
         url: "get_traits",
         success: async function (data) {
-            traitObjects = data;
-            traitsList = Object.getOwnPropertyNames(traitObjects)            
-            
-            //populate the dropdown
+            traitsList = data;
             var selector = document.getElementById("traitSelect");
             for (i=0; i<traitsList.length; i++) {
                 var opt = document.createElement('option')
-                    opt.appendChild(document.createTextNode(traitsList[i]))
-                    opt.value = traitsList[i]
-                    selector.appendChild(opt);
-                    selector.html(document.multiselect('#traitSelect'));
+                opt.appendChild(document.createTextNode(formatHelper.formatForWebsite(traitsList[i])))
+                opt.value = traitsList[i]
+                selector.appendChild(opt);
+                selector.html(document.multiselect('#traitSelect'));
             }
         },
         error: function (XMLHttpRequest) {
@@ -106,7 +101,7 @@ function getAllAssociations (pValue, refGen) {
 
 function getSelectStudyAssociationsByTraits(pValue, refGen) {
     var trait = traitSelectElement.options[traitSelectElement.selectedIndex].value;
-    trait = formatter.formatForTableName(trait);
+    trait = formatHelper.formatForTableName(trait);
     var studyIDs = selectedStudies;
 
     $.ajax({
