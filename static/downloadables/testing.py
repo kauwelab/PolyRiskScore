1592c1dbda3a44bff61d989821c5e6927c6cfc48
Maddy <madmadsoccergirl@hotmail.com>
2020-06-22 20:14:04 -0600

    More work on the CLI

--- static/downloadables/testing.py
+++ static/downloadables/testing.py
@@ -3,10 +3,7 @@ import json
 
 def getAllAssociations(pValue, refGen, traits = None): 
     if (traits is None):
-        url_t = "https://prs.byu.edu/get_traits"
-        response = requests.get(url=url_t)
-        response.close()
-        traits = response.json()
+        traits = getAllTraits()
     associations = {}
     url_a = "https://prs.byu.edu/all_associations"
     h=0
@@ -20,7 +17,7 @@ def getAllAssociations(pValue, refGen, traits = None):
         response = requests.get(url=url_a, params=params)
         response.close()
         if (response):
-            associations = {**response.json(), **associations}
+            associations = {**associations, **response.json()}
             h = i
         else:
             print(response.status_code)
@@ -34,7 +31,7 @@ def getAllAssociations(pValue, refGen, traits = None):
         }
         response = requests.get(url=url_a, params=params)
         response.close()
-        associations = {**response.json(), **associations}
+        associations = {**associations, **response.json()}
     
     snps = "-e #CHROM "
     print(associations)
@@ -50,11 +47,93 @@ def getAllAssociations(pValue, refGen, traits = None):
 
 def getSpecificAssociations(pValue, refGen, traits = None, studyTypes = None, studyIDs = None, ethnicity = None):
     traitData = {}
+    studyIDspecificData = {}
+    url_s = "https://prs.byu.edu/get_studies"
     if (studyIDs is not None):
-        # get the studies and traits associatioed with IDS
+        url_get_by_study_id = "https://prs.byu.edu/get_studies_by_id"
+        params = {
+            "ids": studyIDs
+        }
+        studyIDspecificData = urlWithParams(url_get_by_study_id, params)
+        print(studyIDspecificData)
+    if traits is None and studyTypes is not None:
+        traits = getAllTraits()
+        params = {
+            "traits": traits,
+            "studyTypes": studyTypes
+        }
+        traitData = {**traitData, **urlWithParams(url_s, params)}
+        # get all the traits
+    elif traits is None and studyTypes is None and studyIDs is not None:
+        pass
+        # we are just going to be working with the studyID studies
+    elif traits is not None:
+        if studyTypes is None:
+            params = {
+                "traits": traits,
+                "studyTypes": ["HI", "LC", "O"]
+            }
+        else: 
+            params = {
+                "traits": traits,
+                "studyTypes": studyTypes
+            }
+        traitData = {**traitData, **urlWithParams(url_s, params)}
+    else:
+        # I don't think we should ever make it here...
+        pass
+
+    if traitData:
+        # filter for ethnicity
+        finalTraitList = {}
+        if ethnicity:
+            # print(traitData)
+            for trait in traitData:
+                tmpStudyHolder = []
+                for study in traitData[trait]:
+                    if (ethnicity.lower() in study["ethnicity"].lower()):
+                        tmpStudyHolder.append(study["studyID"])
+                        # print(study["ethnicity"])
+                finalTraitList = {**finalTraitList, **{trait: tmpStudyHolder}}
+            print(finalTraitList)
+        else:
+            finalTraitList = {**finalTraitList, **traitData}
+
+    if studyIDspecificData:
+        for obj in studyIDspecificData:
+            if obj["trait"] in finalTraitList:
+                finalTraitList[obj["trait"]].append(obj["studyID"])
+            else:
+                finalTraitList[obj["trait"]] = [obj["studyID"]]
+
+
+def getAllTraits():
+    url_t = "https://prs.byu.edu/get_traits"
+    response = requests.get(url=url_t)
+    response.close()
+    return response.json()
+
+def urlWithParams(url, params):
+    response = requests.get(url=url, params=params)
+    response.close()
+    if (response):
+        return response.json()
+    else:
+        # throw an error
         pass
     
 
 # getAllAssociations(0.0000000005, "hg38") #, ["Alzheimer's Disease", "acne"])
+getSpecificAssociations(0.00000005, "hg38", ["Alzheimer's Disease", "acne"], ["O"], ethnicity="east Asian")
+# getSpecificAssociations(0.00000005, "hg38", studyIDs=["GCST004246", "GCST002954"]) # can't test until I update the API
+
+# what I'm thinking currently:
+#   if they give us studyIDs -> we grab those no matter what
+#   if they give us IDs without anything else, that is ALL we will use to calculate.
+#   if they give us studyTypes without traits, we will run on all the traits and give the specified study types
+#   if they give us traits without study types, we give all they studyTypes
+#   ethnicity is used to filter the results EXCEPT the ones from the studyIDs
+#   if they give us just ethnicity, we will run for ALL the studies with that specified ethnicity
+
 
 
