021680dbe6a2fe25dac24496770f5d78445caee7
Maddy <madmadsoccergirl@hotmail.com>
2020-08-18 16:12:29 -0600

    added get by studyID endpoint

--- static/js/routes/routes.js
+++ static/js/routes/routes.js
@@ -18,6 +18,9 @@ module.exports = app => {
     // Retrieves study general data for specified studies
     app.get("/get_studies", studies.getFiltered);
 
+    // Retrieves study data using a list of study ids
+    app.get("/get_studies_by_id", studies.getStudyByID);
+
     //searches for study titles or citations containing the given search string
     app.get("/find_studies/:searchStr", studies.findStudies);
 
