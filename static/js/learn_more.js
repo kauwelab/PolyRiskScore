f8605e8a92ef90722f4b6543d7afab7749f6f3af
Matt Hodgman <matthodg1@gmail.com>
2020-08-24 15:05:27 -0600

    Update learn_more.js

--- static/js/learn_more.js
+++ static/js/learn_more.js
@@ -1,8 +1,11 @@
 function displayPanel(elID){
     var x = document.getElementById(elID);
+    var x_sign = document.getElementById(elID + "_sign");
     if (x.style.height === "0px") {
       x.style.height = "100px";
+      x_sign.className = "fa fa-angle-up"
     } else {
       x.style.height = "0px";
+      x_sign.className = "fa fa-angle-down"
     }
 }
