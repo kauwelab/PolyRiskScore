80329968b0e78a3e66a56f382e27ee37c79877da
Maddy <madmadsoccergirl@hotmail.com>
2020-09-15 22:14:52 -0600

    Code adds the reference allele to the snpsData

--- update_database_scripts/createSampleVCF.py
+++ update_database_scripts/createSampleVCF.py
@@ -13,9 +13,18 @@ for obj in snpsData:
 
 # 
 mv = myvariant.MyVariantInfo()
-dictOfRefAlleles = mv.querymany(snps, scopes='dbsnp.rsid', fields='dbsnp.ref')
+queryResults = mv.querymany(snps, scopes='dbsnp.rsid', fields='dbsnp.ref')
+snpRefAlleleDict = {}
 
-for thing in dictOfRefAlleles:
-    print(thing)
+for line in queryResults:
+    if line['query'] not in snpRefAlleleDict.keys():
+        if "notfound" in line.keys():
+            snpRefAlleleDict[line['query']] = '.'
+        else:
+            snpRefAlleleDict[line['query']] = line['dbsnp']['ref']
+
+for i in range(len(snpsData)):
+    snpsData[i]['refAllele'] = snpRefAlleleDict[snpsData[i]['snp']]
+
 
 
