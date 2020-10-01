eaa6465a3552734018e69f541c2e77a7c3a3821a
Maddy <madmadsoccergirl@hotmail.com>
2020-10-01 12:48:11 -0600

    Updated the clumps model fixed the 'clumps' object added getClumpsByPos and getClumpsBySnp

--- static/js/models/clump.model.js
+++ static/js/models/clump.model.js
@@ -1,10 +1,8 @@
 const sql = require('./database')
 
 const Clump = function(mclump) {
-    this.studyID = mclump.studyID,
-    this.trait = mclump.trait,
     this.snp = mclump.snp,
-    this.hg38_pos = mclump.hg38_pos,
+    this.pos = mclump.pos,
     this.african_Clump = mclump.african_Clump,
     this.american_Clump = mclump.american_Clump,
     this.eastAsian_Clump = mclump.eastAsian_Clump,
@@ -12,21 +10,51 @@ const Clump = function(mclump) {
     this.southAsian_Clump = mclump.southAsian_Clump
 }
 
-Clump.getClumps = (studyIDs, superpopclump, result) => {
-    for (i=0; i < studyIDs.length; i++) {
-        studyIDs[i] = "\"" + studyIDs[i] + "\"";
-    }
-
-    sql.query(`SELECT studyID, snp, hg38_pos, ${superpopclump} AS clumpNumber FROM clumps WHERE studyID IN (${studyIDs})`, (err, res) => {
+Clump.getClumps = (superpopclump, refGenome, result) => {
+    sql.query(`SELECT snp, position, ${superpopclump} AS clumpNumber FROM ${refGenome}_clumps`, (err, res) => {
         if (err) {
             console.log("error: ", err);
             result(null, err);
             return;
         }
 
-        console.log(`Clumps quetied for ${studyIDs}, ${res.length} result(s)`);
+        console.log(`Clumps queried for ${superpopclump}, ${res.length} result(s)`);
         result(null, res);
     });
 };
+
+Clump.getClumpsByPos = (superpopclump, refGenome, positions, result) => {
+    for (i=0; i < positions.length; i++) {
+        positions[i] = "\"" + positions[i] + "\"";
+    }
+
+    sql.query(`SELECT snp, position, ${superpopclump} AS clumpNumber FROM ${refGenome}_clumps WHERE position IN (${positions})`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        console.log(`Clumps queried by position for ${superpopclump}; ex: ${positions[0]} - ${res.length} result(s)`);
+        result(null, res);
+    });
+}
+
+Clump.getClumpsBySnp = (superpopclump, refGenome, snps, result) => {
+    for (i=0; i < snps.length; i++) {
+        snps[i] = "\"" + snps[i] + "\"";
+    }
+
+    sql.query(`SELECT snp, position, ${superpopclump} AS clumpNumber FROM ${refGenome}_clumps WHERE snp IN (${snps})`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        console.log(`Clumps queried by snp for ${superpopclump}, ex: ${snp[0]} - ${res.length} result(s)`);
+        result(null, res);
+    });
+}
 
 module.exports = Clump;
