a4d3c9ceaf2aa45d2f61cb5ec46603dfd6a49804
Maddy <madmadsoccergirl@hotmail.com>
2020-10-08 12:32:54 -0600

    Updated connect_to_server to use new endpoints - still need to test

--- static/downloadables/connect_to_server.py
+++ static/downloadables/connect_to_server.py
@@ -5,8 +5,9 @@ import requests
 import time
 import datetime
 
-def retrieveAssociations(pValue, refGen, traits, studyTypes, studyIDs, ethnicity, fileType, superPop):
-
+# get the associations and clumps from the Server
+def retrieveAssociationsAndClumps(pValue, refGen, traits, studyTypes, studyIDs, ethnicity, superPop):
+    # Format variables used for getting associations
     traits = traits.split(" ") if traits != "" else None
     if traits is not None:
         traits = [sub.replace('_', ' ') for sub in traits]
@@ -14,133 +15,86 @@ def retrieveAssociations(pValue, refGen, traits, studyTypes, studyIDs, ethnicity
     studyIDs = studyIDs.split(" ") if studyIDs != "" else None
     ethnicity = ethnicity.split(" ") if ethnicity != "" else None
 
-    if (studyTypes is None and studyIDs is None and ethnicity is None):
-        toReturn = getAllAssociations(pValue, refGen, fileType, traits)
+    # if the user didn't give anything to filter by, get all the associations
+    if (traits is None and studyTypes is None and studyIDs is None and ethnicity is None):
+        toReturn = getAllAssociations(pValue, refGen)
+    # else get the associations using the given filters
     else:
-        toReturn = getSpecificAssociations(pValue, refGen, fileType, traits, studyTypes, studyIDs, ethnicity)
+        toReturn = getSpecificAssociations(pValue, refGen, traits, studyTypes, studyIDs, ethnicity)
     
-    toReturn.append(json.dumps(getClumps(toReturn[2], superPop)))
-    toReturn.pop(1)
+    # get clumps using the refGen and superpopulation
+    toReturn.append(getClumps(refGen, superPop))
     
     print('%'.join(toReturn))
 
 
-def getAllAssociations(pValue, refGen, fileType, traits = None): 
-    if traits is None:
-        traits = getAllTraits()
-    associations = getAssociations("https://prs.byu.edu/all_associations", traits, pValue, refGen)
-    return [json.dumps(associations), getStudyIDs(associations)]
+# gets all associations from the Server
+def getAllAssociations(pValue, refGen): 
+    params = {
+        "pValue": pValue,
+        "refGen": refGen
+    }
+    associations = getUrlWithParams("https://prs.byu.edu/all_associations", params = params)
+    # Organized with studyIDs as the Keys
+    return json.dumps(associations)
 
 
-def getSpecificAssociations(pValue, refGen, fileType, traits = None, studyTypes = None, studyIDs = None, ethnicity = None):
-    studyIDspecificData = {}
+# gets associations using the given filters
+def getSpecificAssociations(pValue, refGen, traits, studyTypes, studyIDs, ethnicity):
 
-    if (studyIDs is not None):
-        url_get_by_study_id = "https://prs.byu.edu/get_studies_by_id"
-        params = {
-            "ids": studyIDs
-        }
-        studyIDspecificData = urlWithParams(url_get_by_study_id, params)
+    # get the studies matching the parameters
+    body = {
+        "traits": traits, 
+        "studyTypes": studyTypes,
+        "ethnicities": ethnicity
+    }
+    traitData = {**postUrlWithBody("https://prs.byu.edu/get_studies", body=body)}
 
-    if traits is None and studyTypes is not None:
-        traits = getAllTraits()
-    elif traits is not None:
-        if studyTypes is None:
-            studyTypes = ["HI", "LC", "O"]
+    # select the studyIDs of the studies
+    finalStudySet = set()
+    for trait in traitData:
+        for study in traitData[trait]:
+            finalStudySet.add(study["studyID"])
 
-    if traits is not None and studyTypes is not None:
-        params = {
-            "traits": traits, 
-            "studyTypes": studyTypes,
-            "ethnicities": ethnicity
-        }
-        traitData = {**urlWithParams("https://prs.byu.edu/get_studies", params)}
+    # add the specified studyIDs to the set of studyIDs
+    if studyIDs is not None:
+        studyIDs = set(studyIDs)
+        finalStudySet = finalStudySet.union(studyIDs)
 
-    if traitData:
-        finalTraitList = []
-        for trait in traitData:
-            tmpStudyHolder = []
-            for study in traitData[trait]:
-                tmpStudyHolder.append(study["studyID"])
-            
-            traitObj = {
-                "trait": trait,
-                "studies": tmpStudyHolder
-            }
-            finalTraitList.append(traitObj)
+    # get the associations based on the studyIDs
+    body = {
+        "pValue": pValue,
+        "refGen": refGen,
+        "studyIDs": list(finalStudySet)
+    }
 
-    if studyIDspecificData:
-        # this will need to be fixed
-        for obj in studyIDspecificData:
-            if obj["trait"] in finalTraitList and obj["studyID"] not in finalTraitList[obj["trait"]]:
-                finalTraitList[obj["trait"]].append(obj["studyID"])
-            else:
-                finalTraitList[obj["trait"]] = [obj["studyID"]]
-
-    associations = getAssociations("https://prs.byu.edu/get_associations", finalTraitList, pValue, refGen, 1)
-    return [json.dumps(associations), getStudyIDs(associations)]
+    associations = postUrlWithBody("https://prs.byu.edu/get_associations", body=body)
+    return json.dumps(associations)
 
 
-def getAssociations(url, traits, pValue, refGen, turnString = None):
-    associations = {}
-    h=0
-    for i in range(25, len(traits), 25):
-        params = {
-            "traits": json.dumps(traits[h:i]) if turnString else traits[h:i],
-            "pValue": pValue,
-            "refGen": refGen
-        }
-        associations = {**associations, **urlWithParams(url, params)}
-        h = i
-    else:
-        params = {
-            "traits": json.dumps(traits[h:len(traits)]) if turnString else traits[h:len(traits)],
-            "pValue": pValue,
-            "refGen": refGen
-        }
-        associations = {**associations, **urlWithParams(url, params)}
-    return associations
-
-
-def getStudyIDs(associations):
-    studyIDs = []
-
-    for disease in associations:
-        for study in associations[disease]:
-            studyIDs.append(study)
-
-    return studyIDs
-
-
-def getAllTraits():
-    url_t = "https://prs.byu.edu/get_traits"
-    response = requests.get(url=url_t)
+# for POST urls
+def postUrlWithBody(url, body):
+    #TODO still need to test this
+    response = requests.post(url=url, body=body)
     response.close()
     assert (response), "Error connecting to the server: {0} - {1}".format(response.status_code, response.reason) 
-    return response.json()
+    return response.json() 
 
 
-def urlWithParams(url, params):
+# for GET urls
+def getUrlWithParams(url, params):
     response = requests.get(url=url, params=params)
     response.close()
     assert (response), "Error connecting to the server: {0} - {1}".format(response.status_code, response.reason) 
     return response.json()  
 
 
-def getClumps(studyIDs, superPop):
-    h=0
-    res = {}
-    for i in range(25, len(studyIDs), 25):
-        params = {
-            "studyIDs":studyIDs[h:i],
-            "superPop":superPop
-        }
-        res = {**res, **urlWithParams("https://prs.byu.edu/ld_clumping", params)}
-        h = i
-    else:
-        params = {
-            "studyIDs":studyIDs[h:len(studyIDs)],
-            "superPop":superPop
-        }
-        res = {**res, **urlWithParams("https://prs.byu.edu/ld_clumping", params)}
-    return res
+# get clumps using the refGen and superPop
+def getClumps(refGen, superPop):
+    params = {
+        "refGen": refGen,
+        "superPop": superPop
+    }
+
+    clumps = getUrlWithParams("https://prs.byu.edu/ld_clumping", params)
+    return json.dumps(clumps)
