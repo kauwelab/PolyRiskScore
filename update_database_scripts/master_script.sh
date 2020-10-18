06dfe808a5f0f0f39b31c226ea4a4dd8e86f76c4
Matthew Cloward <mattcloward@gmail.com>
2020-10-17 18:21:39 -0600

    updated master script file

--- update_database_scripts/master_script.sh
+++ update_database_scripts/master_script.sh
@@ -2,11 +2,11 @@
 
 # This script calls the other scripts in the folder to:
 #   1. download data from the GWAS catalog, 
-#   2. put the data in association tables,
+#   2. put the data in an association table,
 #   3. create a study table,
 #   4. find new and updated studies and traits, 
-#   5. and upload these studies and traits to the PRSKB database. 
-# It usually takes 6ish hours to complete on the PRSKB server using 8 downloading nodes. Using the command below, it runs in the background, which means
+#   5. and upload the new study and association tables to the PRSKB database. 
+# It usually takes 6ish (TODO test) hours to complete on the PRSKB server using 8 downloading nodes. Using the command below, it runs in the background, which means
 # you can leave the server and it will keep running! To see the output, go to the "output.txt" file specified in the command below as well as the 
 # console_files folder for outputs from the data download nodes (see the unpackDatabaseCommandLine.R script).
 #
@@ -23,8 +23,8 @@ if [ $# -lt 2 ]; then
     echo "  [password]"
     echo "  [number of nodes to download data (default: 1)]"
     echo "  [optional: folder for console output files (default: \"./console_files\")]"
-    echo "  [optional: path to association csvs folder (default: \"../tables/association_tables/\")]"
-    echo "  [optional: path to study table folder (not same as association csvs folder) (default: \"./\")]"
+    echo "  [optional: path to association tsv file folder (default: \"../tables/\")]"
+    echo "  [optional: path to study table tsv folder (default: \"../tables/\")]"
     read -p "Press [Enter] key to quit..."
 # check if $2 is a number
 elif ! [[ "$2" =~ ^[0-9]+$ ]]; then
@@ -48,35 +48,53 @@ else
     numGroups=$2
     # if $3, $4, or $5 aren't populated, set them to default values
     consoleOutputFolder=${3:-"./console_files/"}
-    associationTablesFolderPath=${4:-"../tables/association_tables/"} 
+    associationTableFolderPath=${4:-"../tables/"} 
     studyTableFolderPath=${5:-"../tables/"}
 
 #===============Creating Output Paths========================================================
-    # if the default path doesn't exist, create it
+    # if the default console output folder path doesn't exist, create it
     if [ ! -d $consoleOutputFolder ]; then
         mkdir $consoleOutputFolder
         echo "Default console output folder created at" $consoleOutputFolder
     fi
 
-    # if the default path doesn't exist, create it
-    if [ ! -d $associationTablesFolderPath ]; then
-        mkdir $associationTablesFolderPath
-        echo "Default associations table folder created at" $associationTablesFolderPath
+    # if the default association table folder path doesn't exist, create it
+    if [ ! -d $associationTableFolderPath ]; then
+        mkdir $associationTableFolderPath
+        echo "Default associations table folder created at" $associationTableFolderPath
+    fi
+
+    # if the default study table folder path doesn't exist, create it
+    if [ ! -d $studyTableFolderPath ]; then
+        mkdir $studyTableFolderPath
+        echo "Default associations table folder created at" $studyTableFolderPath
     fi
 
 #===============GWAS Database Unpacker======================================================
+    # download a portion of the study data from the GWAS catalog and put it into tsv files
+    # this makes it so each instance of the "unpackDatabaseCommandLine.R" doesn't need to download its own data
+    studyAndPubTSVFolderPath="."
+    Rscript downloadStudiesToFile.R $studyAndPubTSVFolderPath
+
+    chainFileFolderPath="."
     echo "Running GWAS database unpacker. This will take many hours depending on the number of nodes you specified to download data."
     for ((groupNum=1;groupNum<=numGroups;groupNum++)); do
-        Rscript unpackDatabaseCommandLine.R $associationTablesFolderPath "." $groupNum $numGroups &> "$consoleOutputFolder/output$groupNum.txt" &
+        Rscript unpackDatabaseCommandLine.R $associationTableFolderPath $studyAndPubTSVFolderPath $chainFileFolderPath $groupNum $numGroups &> "$consoleOutputFolder/output$groupNum.txt" &
     done
     wait
-    echo -e "Finished unpacking the GWAS database. The associations tables can be found at" $associationTablesFolderPath "\n"
+    #TODO delete study and pub tsvs  
+    echo -e "Finished unpacking the GWAS database. The associations table can be found at" $associationTableFolderPath "\n"
 
 #===============Study Table Code============================================================
     echo "Creating the study table. This can take an hour or more to complete."
     Rscript createStudyTable.R $outputFilesPath
     wait
     echo -e "Finished creating the study table. It can be found at" $studyTableFolderPath "\n"
+    #TODO test
+    delete the raw study data files after the study table has been created
+    rm "./rawGWASStudyData.tsv"
+    rm "./rawGWASPublications.tsv"
+    rm "./rawGWASAncestries.tsv"
 
 #===============Get Updated Studies and Traits===============
     # get updated studies and traits
@@ -123,7 +141,7 @@ else
         echo -e "No GWAS catalog tables have been updated, so no tables were updated or uploaded to the PRSKB database.\n"
     else
         echo "Uploading tables to the PRSKB database."
-        python3 uploadTablesToDatabase.py "$password" $associationTablesFolderPath $studyTableFolderPath "false" "$updatedTraits"
+        python3 uploadTablesToDatabase.py "$password" $associationTableFolderPath $studyTableFolderPath "false" "$updatedTraits"
         wait
         echo -e "Finished uploading tables to the PRSKB database.\n"
     fi
