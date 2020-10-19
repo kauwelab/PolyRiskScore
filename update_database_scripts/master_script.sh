a907e3751dacbd5e0d081fd57a226fd734a1e2d6
Matthew Cloward <mattcloward@gmail.com>
2020-10-19 09:16:03 -0600

    remade changes to master script by Maddy

--- update_database_scripts/master_script.sh
+++ update_database_scripts/master_script.sh
@@ -95,52 +95,13 @@ else
     rm "./rawGWASPublications.tsv"
     rm "./rawGWASAncestries.tsv"
 
-#===============Get Updated Studies and Traits===============
-    # get updated studies and traits
-    echo "Getting the updated studies and traits lists."
-    output=`python3 getUpdatedStudiesAndTraitsLists.py "$password" "$studyTableFolderPath"`
-    # 1 if there was an error executing getUpdatedStudiesAndTraitsLists.py, 0 if there was no error
-    updatedListsReturnCode=$?
-    # read the output into an array of lines
-    readarray -t arrayOutput <<<"$output"
-
-    # if the getUpdatedStudiesAndTraitsLists exited with errors, print the errors and abort
-    if ! [[ $updatedListsReturnCode == 0 ]]; then
-        echo "The updated studies and traits could not be determined. Aborting."
-        END=${#arrayOutput[@]}
-        for ((i=0;i<=END;i++)); do
-            echo ${arrayOutput[$i]}
-        done
-        read -p "Press [Enter] key to finish..."
-        exit 1
-    fi
-
-    # print all but the last 3 lines, two of which are the updated studies and trait lists and the last is an empty line (not sure why)
-    END=${#arrayOutput[@]}-3
-    for ((i=0;i<=END;i++)); do
-        echo ${arrayOutput[$i]}
-    done
-
-    # capture the study and trait lists as strings and remove \r and \n
-    updatedStudies=$(echo ${arrayOutput[$i]} | sed -e 's/\r\n//g')
-    updatedTraits=$(echo ${arrayOutput[$i+1]} | sed -e 's/\r\n//g')
-
-    # send the updatedStudies to a txt file
-    echo $updatedStudies > updatedStudies.txt
-    # send the updatedTraits to a txt file
-    echo $updatedTraits > updatedTraits.txt
-    echo "Printed out updated studies to updatedStudies.txt and updated traits to updatedTraits.txt for LD clumping script."
-
-    # add a space between sections in the output
-    echo ""
-
 #===============Upload Tables to PRSKB Database========================================================
     # if updatedStudies is empty or none, dont' upload, otherwise upload new tables
     if [ -z "$updatedStudies" ] || [ "$updatedStudies" == "none" ]; then
         echo -e "No GWAS catalog tables have been updated, so no tables were updated or uploaded to the PRSKB database.\n"
     else
         echo "Uploading tables to the PRSKB database."
-        python3 uploadTablesToDatabase.py "$password" $associationTableFolderPath $studyTableFolderPath "false" "$updatedTraits"
+        python3 uploadTablesToDatabase.py "$password" $studyTableFolderPath
         wait
         echo -e "Finished uploading tables to the PRSKB database.\n"
     fi
