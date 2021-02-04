ed461f1d5c90085b41505e31fdc23316324773c7
Matthew Cloward <mattcloward@gmail.com>
2021-02-03 22:33:14 -0700

    updated test file

--- static/images/testing/CLICMDTesting.sh
+++ static/images/testing/CLICMDTesting.sh
@@ -12,60 +12,67 @@ for i in "sample.vcf" "sample.txt"; do
     ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test0.csv" -c $pvalue -r $refGen -p $pop
 
     # test 1 (-t -k -i -e -v -g -s)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test1.csv" -c $pvalue -r $refGen -p $pop -t "acne" -k "HI" -i "GCST007234" -e "european" -v -g "Male" -s
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test1.csv" -c $pvalue -r $refGen -p $pop -t "acne" -k "HI" -i "GCST007234" -e "european" -v -g "Male" -s "1"
 
     # TODO test 2 (do test 1 but without internet)
 
     # test 3 (-t)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test3.csv" -c $pvalue -r $refGen -p $pop -t "Autosomal Dominant Compelling Helio-Ophthalmic Outburst Syndrome"
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test3.csv" -c $pvalue -r $refGen -p $pop -t "Asthma"
 
-    # test 4 (2 traits using -t0
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test4.csv" -c $pvalue -r $refGen -p $pop -t "Yu-Zhi Constitution Type" -t "Ischemic Cardiomyopathy"
+    # test 4 (-t again)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test4.csv" -c $pvalue -r $refGen -p $pop -t "Alzheimer's Disease"
 
-    # TODO test 5 (do test 3, but without internet)
+    # test 5 (2 traits using -t)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test5.csv" -c $pvalue -r $refGen -p $pop -t "Asthma" -t "insomnia"
 
-    # test 6 (-k)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test6.csv" -c $pvalue -r $refGen -p $pop -k "LC"
+    # TODO test 6 (do test 3, but without internet)
 
-    # test 7 (2 study types using -k)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test7.csv" -c $pvalue -r $refGen -p $pop -k "LC" -k "HI"
+    # test 7 (-k)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test7.csv" -c $pvalue -r $refGen -p $pop -k "LC"
 
-    # TODO test 8 (do test 6, but without internet)
+    # test 8 (2 study types using -k)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test8.csv" -c $pvalue -r $refGen -p $pop -k "LC" -k "O"
 
-    # test 9 (-i)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test9.csv" -c $pvalue -r $refGen -p $pop -i "GCST003378"
+    # TODO test 9 (do test 7, but without internet)
 
-    # test 10 (2 study IDs using -i)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test9.csv" -c $pvalue -r $refGen -p $pop -i "GCST007897" -i "GCST90000255"
+    # test 10 (-i)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test10.csv" -c $pvalue -r $refGen -p $pop -i "GCST000001"
 
-    # TODO test 11 (do test 9, but without internet)
+    # test 11 (-i, but the id is not in our database)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test11.csv" -c $pvalue -r $refGen -p $pop -i "GCST000003"
 
-    # test 12 (-e)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test12.csv" -c $pvalue -r $refGen -p $pop -e "Sub-Saharan African"
+    # test 12 (2 study IDs using -i)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test12.csv" -c $pvalue -r $refGen -p $pop -i "GCST000001" -i "GCST000010"
 
-    # test 13 (2 ethnicities using -e)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test13.csv" -c $pvalue -r $refGen -p $pop -e "Unspecified" -e "Other"
+    # TODO test 13 (do test 10, but without internet)
 
-    # TODO test 14 (do test 11, but without internet)
+    # test 14 (-e)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test14.csv" -c $pvalue -r $refGen -p $pop -e "East Asian"
 
-    # test 15 (-v)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test15.csv" -c $pvalue -r $refGen -p $pop -v "true"
+    # test 15 (2 ethnicities using -e)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test15.csv" -c $pvalue -r $refGen -p $pop -e "East Asian" -e "european"
 
-    # TODO test 16 (do test 15, but without internet)
+    # TODO test 16 (do test 14, but without internet)
 
-    # test 17 (-g)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test17.csv" -c $pvalue -r $refGen -p $pop -g "Male"
+    # test 17 (-v)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test17.csv" -c $pvalue -r $refGen -p $pop -v "true"
 
     # TODO test 18 (do test 17, but without internet)
 
-    # test 19 (-s)
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test19.csv" -c $pvalue -r $refGen -p $pop -s "1"
-    # test 19.5
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test19.5.csv" -c $pvalue -r $refGen -p $pop -s "2"
+    # test 19 (-g)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test19.csv" -c $pvalue -r $refGen -p $pop -g "Male"
 
     # TODO test 20 (do test 19, but without internet)
 
-    # test 21 
-    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test21.csv" -c $pvalue -r $refGen -p $pop -t "Vitiligo" -k "O"
+    # test 21 (-s)
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test21.csv" -c $pvalue -r $refGen -p $pop -s "1"
+
+    # test 21.5
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test21.5.csv" -c $pvalue -r $refGen -p $pop -s "2"
+
+    # TODO test 22 (do test 21, but without internet)
+
+    # test 23 
+    ./runPrsCLI.sh -f $inputFilePath -o "$outputFolder/test23.csv" -c $pvalue -r $refGen -p $pop -t "Insomnia" -k "LC"
 done
 
