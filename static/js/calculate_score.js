fb54024a22b27e319b90475ca64f4c807c7d72b5
Matthew Cloward <mattcloward@gmail.com>
2021-02-08 15:07:05 -0700

    fixed condensed header not printing properly

--- static/js/calculate_score.js
+++ static/js/calculate_score.js
@@ -588,7 +588,7 @@ function formatCSV(jsonObject, isCondensed) {
                 first = false
                 sampleKeys = Object.keys(jsonObject['studyResults'][studyID]['traits'][trait])
                 if (isCondensed) {
-                    resultsString = headerInit.toString().concat(sampleKeys.toString())
+                    resultsString = headerInit.concat(sampleKeys.toString())
                 }
                 else {
                     resultsString = headerInit.toString()
