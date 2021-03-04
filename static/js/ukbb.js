57326bdf5c94a9c944d71a605467419a4ebb91f1
Maddy <madmadsoccergirl@hotmail.com>
2021-03-04 12:19:50 -0700

    Fill table with summary values

--- static/js/ukbb.js
+++ static/js/ukbb.js
@@ -124,17 +124,14 @@ function displayGraphs() {
                 changePlot()
 
                 var values = [
-                    ['Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'],
-                    [1200000, 20000, 80000, 2000, 12120000],
-                    [1300000, 20000, 70000, 2000, 130902000],
-                    [1300000, 20000, 120000, 2000, 131222000],
-                    [1400000, 20000, 90000, 2000, 14102000]]
+                    ['Min', 'Max', 'Mean', 'Median', 'Range'],
+                    [displayDataObj["min"], displayDataObj["max"], displayDataObj["mean"], displayDataObj["median"], displayDataObj["rng"]]
+                ]
 
                 var tableData = [{
                     type: 'table',
                     header: {
-                        values: [["<b>EXPENSES</b>"], ["<b>Q1</b>"],
-                            ["<b>Q2</b>"], ["<b>Q3</b>"], ["<b>Q4</b>"]],
+                        values: [["<b>Summary Values</b>"], ["<b>" + displayDataObj["studyID"] + "</b>"]],
                         align: "center",
                         line: {width: 1, color: 'black'},
                         fill: {color: "grey"},
