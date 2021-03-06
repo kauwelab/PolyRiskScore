b6fa81c9b9ecd8fda1b8859fdc0bb219bcb3cd01
evance3 <evance3@lsprs.byu.edu>
2021-03-06 16:59:06 -0700

    fixed duplicate removal statement

--- static/downloadables/runPrsCLI.sh
+++ static/downloadables/runPrsCLI.sh
@@ -651,7 +651,6 @@ calculatePRS () {
         # TODO I've never tested this with running multiple iterations. I don't know if this is something that would negativly affect the tool
         rm ".workingFiles/filteredInput_${TIMESTAMP}${extension}"
         rm -r __pycache__
-        rm ".workingFiles/filteredInput_${TIMESTAMP}${extension}"
         echo "Cleaned up intermediate files"
         echo -e "Finished. Exiting...\n\n"
         exit;
