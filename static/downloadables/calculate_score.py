921aedfce566b03604b5fc58051f7714983e09c6
evance3 <evance3@lsprs.byu.edu>
2021-03-05 17:39:01 -0700

    removed print statements

--- static/downloadables/calculate_score.py
+++ static/downloadables/calculate_score.py
@@ -267,9 +267,6 @@ def formatJson(isFirst, studyInfo, outputFile):
 def createMarks(oddsRatios, studyID, snpSet, sampSnps, mark):
     prs = str(getPRSFromArray(oddsRatios))
     # Add an * to scores that don't include every snp in the study
-    #TODO: check this
-    print(set(snpSet))
-    print(set(sampSnps))
     if set(snpSet) != sampSnps and len(sampSnps) != 0:
         prs = prs + '*'
     # Add a mark to studies that have duplicate snps with varying pvalue annotations
