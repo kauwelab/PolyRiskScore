1a6cbecf0573d9f1ccea2f4569443917f53796ac
Maddy <madmadsoccergirl@hotmail.com>
2021-03-11 12:55:45 -0700

    delete the vcf_reader before removing the file

--- static/downloadables/parse_associations.py
+++ static/downloadables/parse_associations.py
@@ -322,6 +322,8 @@ def parse_vcf(filteredFilePath, clumpsObjDict, tableObjDict, snpSet, clumpNumDic
     if snpCount == 0:
         unusedTraitStudy = True
         if os.path.exists(tempFilePath):
+            # delete the vcf_reader so when we go to remove the tempFilePath, we don't get an error that the file is open
+            del vcf_reader
             os.remove(tempFilePath)
         return sample_map, neutral_snps_map, clumped_snps_map, sample_num, unusedTraitStudy, sampleOrder
     else:
