8a29e2a27ff538c67ff7db011d225e38674eba7f
Matthew Cloward <mattcloward@gmail.com>
2021-03-12 20:57:08 -0700

    message for finishing study table moved

--- update_database_scripts/createStudyTable.R
+++ update_database_scripts/createStudyTable.R
@@ -259,4 +259,5 @@ if (is_ebi_reachable()) {
   stop("The EBI API is unreachable. Check internet connection and try again.", call.=FALSE)
 }
 
+print(paste0("Finished unpacking the GWAS database. The associations table can be found at", associationTablePath, "\n")
 print(paste0("Total time elapsed: ", format(Sys.time() - start_time)))
