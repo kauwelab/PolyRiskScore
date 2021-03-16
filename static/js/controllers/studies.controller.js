014ae7e8e2b55503416e30166d57a276b960eef2
Maddy <madmadsoccergirl@hotmail.com>
2021-03-16 11:34:30 -0600

    added endpoint

--- static/js/controllers/studies.controller.js
+++ static/js/controllers/studies.controller.js
@@ -169,3 +169,21 @@ exports.findStudies = (req, res) => {
         }
     });
 };
+
+exports.downloadStudyTable = (req, res) => {
+    downloadPath = path.join(__dirname, '../../..', 'tables')
+    var options = {
+        root: downloadPath
+    };
+    var fileName = 'study_table.tsv';
+    res.sendFile(fileName, options, function (err) {
+        if (err) { 
+            console.log(err); 
+            res.status(500).send({
+                message: "Error finding file"
+            });
+        } else { 
+            console.log('Sent:', fileName); 
+        }
+    });
+}
