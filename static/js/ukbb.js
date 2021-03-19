8867c3c904fbeb03dde0cbd72a2a87827ecb959c
Maddy <madmadsoccergirl@hotmail.com>
2021-03-19 15:36:37 -0600

    updeted ukbbpage formatting

--- static/js/ukbb.js
+++ static/js/ukbb.js
@@ -125,7 +125,13 @@ function displayGraphs() {
                 displayTable()
 
                 var studyMetadata = document.getElementById("studymetadata")
-                metadatastring = `<p><b>Title:</b> ${selectedStudy.getAttribute("data-title")}</p><p><b>Citation:</b> ${selectedStudy.getAttribute("data-citation")}</p><p><b>Trait:</b> ${selectedStudy.getAttribute("data-trait")}</p><p><b>Reported Trait:</b> ${selectedStudy.getAttribute("data-reported-trait")}</p><p><b>Pubmed ID:</b> ${selectedStudy.getAttribute("data-pubmedid")}</p><p><b>Altmetric Score:</b> ${selectedStudy.getAttribute("data-altmetric-score")}</p><br>`
+                metadatastring = `<p><b>Title:</b> ${selectedStudy.getAttribute("data-title")}</p>
+                <p><b>Citation:</b> ${selectedStudy.getAttribute("data-citation")}</p>
+                <p><b>Trait:</b> ${selectedStudy.getAttribute("data-trait")}</p>
+                <p><b>Reported Trait:</b> ${selectedStudy.getAttribute("data-reported-trait")}</p>
+                <p><b>Pubmed ID:</b> ${selectedStudy.getAttribute("data-pubmedid")}</p>
+                <p><b>Altmetric Score:</b> ${selectedStudy.getAttribute("data-altmetric-score")}</p>
+                <p><b>UK Biobank SNPs Used:</b><br>`
                 studyMetadata.innerHTML = metadatastring
             }
         },
