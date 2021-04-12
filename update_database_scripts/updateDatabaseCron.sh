4e2a65d3d9c06109bc77ac11bc4e7d1cf0602dc5
Matthew Cloward <mattcloward@gmail.com>
2021-04-12 16:04:16 -0600

    updated documentation

--- update_database_scripts/updateDatabaseCron.sh
+++ update_database_scripts/updateDatabaseCron.sh
@@ -1,2 +1,9 @@
+# This script is run the first of every month on the PRSKB server through a cron job. To edit the cron job, which is found in roots cron jobs, use the
+# following command: sudo crontab -e -u root
+# The line containing the file name of this file is the line to change. For a better description of the cron job, see the master script readme file.
+#
+# How to run: ./updateDatabasecron.sh "filePath"
+# where: "filePath" is the path to the passwords file
+
 # runs the master script with the password path specified by $1 with 8 subprocesses downloading association data
 ./master_script.sh $1 8
