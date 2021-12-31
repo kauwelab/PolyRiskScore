280cdd9e5326aa56760bca552ebf128916aef7a0
Maddy <madmadsoccergirl@hotmail.com>
2021-12-31 17:04:41 -0500

    added ogValue Types to the snps to traitStudyID

--- static/js/models/association.model.js
+++ static/js/models/association.model.js
@@ -232,7 +232,7 @@ Association.getSnpsToTraitStudyID = (studyIDObjs, result) => {
             if (!(Object.prototype.toString.call(studyObj) === '[object Object]')) {
                 studyObj = JSON.parse(studyObj)
             }
-            queryString = queryString.concat(`SELECT snp, studyID, trait, pValueAnnotation, betaAnnotation FROM associations_table WHERE studyID = ? AND trait = ? AND pValueAnnotation = ? AND betaAnnotation = ?; `)
+            queryString = queryString.concat(`SELECT snp, studyID, trait, pValueAnnotation, betaAnnotation, ogValueTypes FROM associations_table WHERE studyID = ? AND trait = ? AND pValueAnnotation = ? AND betaAnnotation = ?; `)
             queryParams = queryParams.concat([studyObj.studyID, studyObj.trait, studyObj.pValueAnnotation, studyObj.betaAnnotation])
         })
         console.log('about to query table')
