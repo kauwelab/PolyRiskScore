a02ec58ade94bb0a4612a135a8f2d617716f1d8b
Maddy <madmadsoccergirl@hotmail.com>
2022-02-23 14:30:45 -0500

    fixed wrong thing

--- static/downloadables/calculate_score.py
+++ static/downloadables/calculate_score.py
@@ -55,35 +55,14 @@ def txtcalculations(snpSet, txtObj, tableObjDict, mafDict, isJson, isCondensedFo
                                     riskVariants.add(snp)
                             elif allele == "." :
                                 mafVal = mafDict[snp]['alleles'][riskAllele] if snp in mafDict and riskAllele in mafDict[snp]["alleles"] else 0
-                                mafKey = "{}|{}".format(mafVal, riskAllele)
-                                # We can't put this into the betas yet, because if there are multiple reported risk alleles (uncommon, but possible) we need to be able to chose the allele with the most common allele frequency
-                                if mafKey not in tmpMafDeciderObj:
-                                    tmpMafDeciderObj[mafKey] = {
-                                        "snpBeta": snpBeta,
-                                        "numAlleles": 1
-                                    }
-                                else:
-                                    tmpMafDeciderObj[mafKey]["numAlleles"] += 1
+                                betas.append(snpBeta*mafVal)
+                                betaUnits.add(units)
+                                if snpBeta < 0:
+                                    protectiveVariants.add(snp)
+                                elif snpBeta > 0:
+                                    riskVariants.add(snp)
                             else:
                                 unmatchedAlleleVariants.add(snp)
-                
-                # here is where we find the allele with the greatest allele frequency, which indicates that the allele is more prevelant in the population and should thus be the one we choose to use (note, most of the time, there is only a single allele being considered here)
-                tmpMAFmostCommonKey = ""
-                tmpMAFmostCommonVal = 0
-                tmpMAFmostCommonBeta = 0
-                tmpMAFmostCommonUnits = ""
-                for key in tmpMafDeciderObj:
-                    maf, mafAllele = key.split("|")
-                    if maf >= tmpMAFmostCommonVal:
-                        tmpMAFmostCommonVal = maf
-                        tmpMAFmostCommonKey = key
-                        tmpMAFmostCommonBeta = tmpMafDeciderObj[key]["snpBeta"]
-                betas.append(tmpMAFmostCommonBeta*tmpMAFmostCommonVal*tmpMafDeciderObj[tmpMAFmostCommonKey]["numAlleles"])
-                betaUnits.add(tmpMAFmostCommonUnits)
-                if tmpMAFmostCommonBeta < 0:
-                    protectiveVariants.add(snp)
-                elif tmpMAFmostCommonBeta > 0:
-                    riskVariants.add(snp)
 
         if len(betaUnits) > 1:
             lowercaseB = [x.lower() for x in betaUnits]
@@ -176,7 +155,6 @@ def vcfcalculations(snpSet, vcfObj, tableObjDict, mafDict, isJson, isCondensedFo
                     # check if the snp is in this trait/study
                     if rsID in snpSet:
                         nonMissingSnps += 1
-                        tmpMafDeciderObj = {}
                         for riskAllele in tableObjDict['associations'][rsID]['traits'][trait][studyID][pValBetaAnnoValType]:
                             units = tableObjDict['associations'][rsID]["traits"][trait][studyID][pValBetaAnnoValType][riskAllele]['betaUnit']
                             alleles = vcfObj[samp][rsID]
@@ -195,34 +173,14 @@ def vcfcalculations(snpSet, vcfObj, tableObjDict, mafDict, isJson, isCondensedFo
                                                 riskVariants.add(rsID)
                                         elif allele == ".":
                                             mafVal = mafDict[rsID]['alleles'][riskAllele] if rsID in mafDict and riskAllele in mafDict[rsID]["alleles"] else 0
-                                            mafKey = "{}|{}".format(mafVal, riskAllele)
-                                            # We can't put this into the betas yet, because if there are multiple reported risk alleles (uncommon, but possible) we need to be able to chose the allele with the most common allele frequency
-                                            if mafKey not in tmpMafDeciderObj:
-                                                tmpMafDeciderObj[mafKey] = {
-                                                    "snpBeta": snpBeta,
-                                                    "numAlleles": 1
-                                                }
-                                            else:
-                                                tmpMafDeciderObj[mafKey]["numAlleles"] += 1
+                                            betas.append(snpBeta*mafVal)
+                                            betaUnits.add(units)
+                                            if snpBeta < 0:
+                                                protectiveVariants.add(rsID)
+                                            elif snpBeta > 0:
+                                                riskVariants.add(rsID)
                                         else:
                                             unmatchedAlleleVariants.add(rsID)
-                        # here is where we find the allele with the greatest allele frequency, which indicates that the allele is more prevelant in the population and should thus be the one we choose to use (note, most of the time, there is only a single allele being considered here)
-                        tmpMAFmostCommonKey = ""
-                        tmpMAFmostCommonVal = 0
-                        tmpMAFmostCommonBeta = 0
-                        tmpMAFmostCommonUnits = ""
-                        for key in tmpMafDeciderObj:
-                            maf, mafAllele = key.split("|")
-                            if maf >= tmpMAFmostCommonVal:
-                                tmpMAFmostCommonVal = maf
-                                tmpMAFmostCommonKey = key
-                                tmpMAFmostCommonBeta = tmpMafDeciderObj[key]["snpBeta"]
-                        betas.append(tmpMAFmostCommonBeta*tmpMAFmostCommonVal*tmpMafDeciderObj[tmpMAFmostCommonKey]["numAlleles"])
-                        betaUnits.add(tmpMAFmostCommonUnits)
-                        if tmpMAFmostCommonBeta < 0:
-                            protectiveVariants.add(rsID)
-                        elif tmpMAFmostCommonBeta > 0:
-                            riskVariants.add(rsID)
 
             if len(betaUnits) > 1:
                 lowercaseB = [x.lower() for x in betaUnits]
