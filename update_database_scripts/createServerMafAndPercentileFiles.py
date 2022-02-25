1fff646a13c3f0d26e1867a31578ddc2bf85f11a
mattcloward <mattcloward@gmail.com>
2022-02-25 15:16:29 -0700

    updated maf download files and other server download files

--- update_database_scripts/createServerMafAndPercentileFiles.py
+++ update_database_scripts/createServerMafAndPercentileFiles.py
@@ -6,13 +6,14 @@ from numpy import percentile
 import requests
 from multiprocessing import Pool
 from uploadTablesToDatabase import checkTableExists, getConnection
+import sys
 
 # This script creates associations files and clumps files for download to the CLI
 #
 # How to run: python3 createServerMafAndPercentileFiles.py "password"
 # where "password" is the password to the PRSKB database
 
-# This script should be run at least once yearly, after we have updated the maf and cohorts
+# This script should be run monthly as well
 
 
 # gets the percentiles from the database
@@ -72,33 +73,77 @@ def formatPercentiles(percentilesUnformatted):
 
 
 # grab MAF data from the database to put into a txt file for download
-def getMAF(tablePrefix, config): #, snps):
-
-    connection = getConnection(config)
-
+def getMAF(tablePrefix, config, generalFilePath):
+    hg17 = open(os.path.join(generalFilePath, "{tablePrefix}_hg17.txt".format(tablePrefix=tablePrefix)), "w")
+    hg18 = open(os.path.join(generalFilePath, "{tablePrefix}_hg18.txt".format(tablePrefix=tablePrefix)), "w")
+    hg19 = open(os.path.join(generalFilePath, "{tablePrefix}_hg19.txt".format(tablePrefix=tablePrefix)), "w")
+    hg38 = open(os.path.join(generalFilePath, "{tablePrefix}_hg38.txt".format(tablePrefix=tablePrefix)), "w")
+    hg17.write("{ ")
+    hg18.write("{ ")
+    hg19.write("{ ")
+    hg38.write("{ ")
     mafUnformatted = []
-    for i in range(1, 23):
-        tableName = tablePrefix + "_chr{i}".format_map(i=i)
-        if (checkTableExists(connection.cursor(), tableName)):
-            cursor = connection.cursor()
-            sql = "SELECT * FROM {table} WHERE snp != 'None'; ".format(tablePrefix) #, snps)
-            cursor.execute(sql)
-            returnedMaf = cursor.fetchall()
-            cursor.close()
-            mafUnformatted.extend(returnedMaf)
-        else:
-            raise NameError('Table does not exist in database: {tablename}'.format(tableName=tableName))
+    connection = getConnection(config)
+    try:
+        for i in range(1, 23):
+            tableName = tablePrefix + "_chr{i}".format(i=i)
+            print(f'on {tableName}')
+            if (checkTableExists(connection.cursor(), tableName)):
+                cursor = connection.cursor()
+                sql = "SELECT {0}.chrom, {0}.hg38, {0}.hg19, {0}.hg18, {0}.hg17, {0}.snp, {0}.allele, {0}.alleleFrequency FROM {0} INNER JOIN associations_table ON {0}.snp=associations_table.snp ORDER BY snp; ".format(tableName)
+                cursor.execute(sql)
+                returnedMaf = cursor.fetchall()
+                #mafUnformatted.extend(returnedMaf)
+                cursor.close()
+                mafhg17, mafhg18, mafhg19, mafhg38 = formatMAF(i,returnedMaf, hg17, hg18, hg19, hg38)
+                if i < 22:
+                    hg17.write(", ")
+                    hg18.write(", ")
+                    hg19.write(", ")
+                    hg38.write(", ")
 
+            else:
+                raise NameError('Table does not exist in database: {tablename}'.format(tablename=tableName))
+    except:
+        print(sys.exc_info()[1])
+    #mafhg17, mafhg18, mafhg19, mafhg38 = formatMAF(mafUnformatted)
+    hg17.write(" }")
+    hg17.close()
+    hg18.write(" }")
+    hg18.close()
+    hg19.write(" }")
+    hg19.close()
+    hg38.write(" }")
+    hg38.close()
     return mafUnformatted
 
 
 # format MAF data correctly
-def formatMAF(mafUnformatted):
+def formatMAF(i, mafUnformatted, hg17_file, hg18_file, hg19_file, hg38_file):
     mafhg17 = {}
     mafhg18 = {}
     mafhg19 = {}
     mafhg38 = {}
+    oldSnp = ""
+    linesToWriteHg17 = []
+    linesToWriteHg18 = []
+    linesToWriteHg19 = []
+    linesToWriteHg38 = []
+    
     for chrom, hg38, hg19, hg18, hg17, snp, allele, alleleFrequency in mafUnformatted:
+        if oldSnp != "" and oldSnp != snp:
+            linesToWriteHg17.append(f'"{oldSnp}": {json.dumps(mafhg17[oldSnp])}')
+            linesToWriteHg18.append(f'"{oldSnp}": {json.dumps(mafhg18[oldSnp])}')
+            linesToWriteHg19.append(f'"{oldSnp}": {json.dumps(mafhg19[oldSnp])}')
+            linesToWriteHg38.append(f'"{oldSnp}": {json.dumps(mafhg38[oldSnp])}')
+            mafhg17 = {}
+            mafhg18 = {}
+            mafhg19 = {}
+            mafhg38 = {}
+            oldSnp = snp
+        elif oldSnp == "":
+            oldSnp = snp
+
         if snp not in mafhg17:
             mafhg17[snp] = {
                 "chrom": chrom,
@@ -125,6 +170,11 @@ def formatMAF(mafUnformatted):
         mafhg19[snp]['alleles'][allele] = alleleFrequency
         mafhg38[snp]['alleles'][allele] = alleleFrequency
 
+    hg17_file.write(", ".join(linesToWriteHg17))
+    hg18_file.write(", ".join(linesToWriteHg18))
+    hg19_file.write(", ".join(linesToWriteHg19))
+    hg38_file.write(", ".join(linesToWriteHg38))
+
     return mafhg17, mafhg18, mafhg19, mafhg38
 
 
@@ -132,6 +182,8 @@ def createMAFDownloadFiles(params):
     tablePrefix = params[0]
     password = params[1]
     generalFilePath = params[2]
+
+    print(f'On {tablePrefix}')
 
     config = {
         'user': 'polyscore',
@@ -141,51 +193,7 @@ def createMAFDownloadFiles(params):
         'auth_plugin': 'mysql_native_password',
     }
 
-    # This would be a good thing to do, but currently we just filter the snps by whether they have an rsID
-    # # get viable snps to pass to the getMAF function
-    # # we grab these so that we can make sure we only have maf for snps in our database
-    # # connection = getConnection(config)
-    # # snps={}
-    # # if (checkTableExists(connection.cursor(), "associations_table")):
-    # #     cursor = connection.cursor()
-    # #     sql = "SELECT DISTINCT snp from associations_table; "
-    # #     cursor.execute(sql)
-    # #     viableSnps = cursor.fetchall()
-    # #     cursor.close()
-    # #     for snp in viableSnps:
-    # #         snps.add(snp)
-    # #     snps = ", ".join(list(snps))
-
-    # # else:
-    # #     raise NameError('Table does not exist in database: associations_table')
-
-    # unformattedMaf = getMAF(tablePrefix, config, snps)
-    unformattedMaf = getMAF(tablePrefix, config)
-    mafhg17, mafhg18, mafhg19, mafhg38 = formatMAF(unformattedMaf)
-
-    hg17 = open(os.path.join(generalFilePath, "{tablePrefix}_hg17.txt".format(tablePrefix=tablePrefix)), "w")
-    hg17.write(json.dumps(mafhg17))
-    hg17.close()
-
-    mafhg17 = {}
-
-    hg18 = open(os.path.join(generalFilePath, "{tablePrefix}_hg18.txt".format(tablePrefix=tablePrefix)), "w")
-    hg18.write(json.dumps(mafhg18))
-    hg18.close()
-
-    mafhg18 = {}
-
-    hg19 = open(os.path.join(generalFilePath, "{tablePrefix}_hg19.txt".format(tablePrefix=tablePrefix)), "w")
-    hg19.write(json.dumps(mafhg19))
-    hg19.close()
-
-    mafhg19 = {}
-
-    hg38 = open(os.path.join(generalFilePath, "{tablePrefix}_hg38.txt".format(tablePrefix=tablePrefix)), "w")
-    hg38.write(json.dumps(mafhg38))
-    hg38.close()
-
-    mafhg38 = {}
+    getMAF(tablePrefix, config, generalFilePath)
 
     return
 
@@ -197,12 +205,14 @@ def main():
 
     # general file path for writing the files to
     generalFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../static/downloadables/preppedServerFiles")
+    print("Starting to get MAF and Percentile data to create files")
 
     # we create params for each table prefix so that we can run them on multiple processes
     for tablePrefix in [ "adni_maf", "ukbb_maf", "afr_maf", "amr_maf", "eas_maf", "eur_maf", "sas_maf" ]:
-        paramMAFopts.append((tablePrefix, password, generalFilePath))
+         paramMAFopts.append((tablePrefix, password, generalFilePath))
+        #createMAFDownloadFiles([tablePrefix, password, generalFilePath])
 
-    with Pool(processes=5) as pool:
+    with Pool(processes=7) as pool:
         pool.map(createMAFDownloadFiles, paramMAFopts)
 
     # COMMENTED OUT UNTIL WE ACTUALLY HAVE PERCENTILES TO WORK WITH
