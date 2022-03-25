e40843504030422ca20905bbb40d8bf9ec3751d2
Maddy <madmadsoccergirl@hotmail.com>
2022-03-25 11:43:26 -0400

    removed extra argv

--- static/downloadables/connect_to_server.py
+++ static/downloadables/connect_to_server.py
@@ -989,4 +989,4 @@ if __name__ == "__main__":
     if argv[1] == "GWAS":
         formatGWASAndRetrieveClumps(argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], argv[10])
     else:
-        retrieveAssociationsAndClumps(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], argv[10], argv[11], argv[12])
+        retrieveAssociationsAndClumps(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], argv[10], argv[11])
