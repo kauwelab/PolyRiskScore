8b07bbe84562498e699a4314158303f9804bbc00
Maddy <madmadsoccergirl@hotmail.com>
2022-03-30 17:46:01 -0400

    fixed path stuff

--- static/downloadables/grep_file.py
+++ static/downloadables/grep_file.py
@@ -121,6 +121,10 @@ def getFilesAndPaths(fileHash, requiredParamsHash, superPop, refGen, isRSids, ti
         tableObjFile = {}
         studySnpsFile = {}
     except FileNotFoundError: 
+        if isFilters:
+            clumpsPath = os.path.join(basePath, "{p}_clumps_{r}_{ahash}.txt".format(p = pop, r = refGen, ahash = fileHash))
+        else:
+            clumpsPath = os.path.join(basePath, "{p}_clumps_{r}.txt".format(p = pop, r = refGen))
         raise SystemExit("ERROR: One or both of the required working files could not be found. \n Paths searched for: \n{0}\n{1}\n{2}".format(associationsPath, clumpsPath, studySnpsPath))
 
     return tableObjDict, allClumps, studySnpsDict, filteredInputPath, clumpNumPath, isFilters
